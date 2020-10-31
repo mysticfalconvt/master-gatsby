dff096f52c9af488d944c5772cdab3028d7d07a9
mysticfalconvt <rboskind@gmail.com>
2020-10-31 14:33:45 -0400

    added basic nav

---
+++ starter-files/gatsby/src/pages/orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+export default function OrdersPage() {
+  return (
+    <div>
+      <Nav />
+      <p> Hey, Im the Orders page</p>
+    </div>
+  );
+}
