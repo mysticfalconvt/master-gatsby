e85673c6cbc3ac2f3d028a441637462116d48f06
Robert Boskind <Robert.Boskind@ncsuvt.org>
2020-11-03 09:27:37 -0500

    finish 11

---
+++ starter-files/sanity/schemas/pizza.js
@@ -0,0 +1,43 @@
+import { MdLocalPizza as icon } from 'react-icons/Md';
+
+export default {
+  // computer name
+  name: 'pizza',
+  // visible title
+  title: 'Pizzas',
+  type: 'document',
+  icon: () => icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the Pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      validation: (Rule) => Rule.min(1000),
+      // todo: add custom component
+    },
+  ],
+};
