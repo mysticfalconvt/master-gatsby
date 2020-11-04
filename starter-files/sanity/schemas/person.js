cd5bed0637e3f97ae56b0088244ac9a18fad776b
Robert Boskind <Robert.Boskind@ncsuvt.org>
2020-11-04 07:54:24 -0500

    done 14

---
+++ starter-files/sanity/schemas/person.js
@@ -0,0 +1,44 @@
+import { MdPerson as icon } from 'react-icons/Md';
+
+export default {
+  // computer name
+  name: 'person',
+  // visible title
+  title: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us a bit about them',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
