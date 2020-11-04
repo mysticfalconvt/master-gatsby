b737e77cca7f470168cf07348b3e42f1b3302847
Robert Boskind <Robert.Boskind@ncsuvt.org>
2020-11-04 07:48:26 -0500

    finish 13

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -39,5 +39,36 @@ export default {
       validation: (Rule) => Rule.min(1000),
       // todo: add custom component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'topping' }],
+        },
+      ],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      console.log(toppings);
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
